b5e6d30d750843a9710612df27c5d034b3e1302f
Ruslan Babkin <ruslan.babkin01@gmail.com>
2022-12-15 11:26:41 +0200

    create atom select

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,19 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/operations';
 import { selectIsLoading, selectError } from 'redux/selectors';
+import { parseJSON } from 'date-fns';
 
 export const App = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
+
+  // const date = '2022-12-14T17:36:34.367Z';
+
+  // const FormatDate = date => {
+  //   return parseJSON(Date.parse(date), 'dd MMMM yyyy, HH:mm');
+  // };
+  // console.log(FormatDate(date));
 
   useEffect(() => {
     dispatch(fetchContacts());
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,15 @@
 import { Contacts, TotalContacts } from './ContactList.styled';
 import { ContactItem } from '../ContactItem/ContactItem';
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilter } from 'redux/selectors';
+import { selectVisibleContacts } from 'redux/selectors';
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-
+  const contacts = useSelector(selectVisibleContacts);
   const totalContacts = contacts.length;
-
-  const normalizedFilter = filter.toLowerCase();
-  const visibleContacts = contacts.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter)
-  );
 
   return (
     <Contacts>
-      {visibleContacts.map(({ phone, name, id }) => (
+      {contacts.map(({ phone, name, id }) => (
         <ContactItem key={id} phone={phone} name={name} id={id} />
       ))}
       <TotalContacts>Total contacts: {totalContacts}</TotalContacts>
